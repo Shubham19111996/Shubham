23bc9c5b36441178ee51677702bf8a1f253b0448
Shubham19111996 <122982779+Shubham19111996@users.noreply.github.com>
2023-01-18 17:37:12 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+<!Document html>
+
+<html>
+
+    <head>
+        <title>Validation Form</title>
+    </head>
+    <body>
+        <form>
+            <input type="text" id="Username" required>
+                </br>
+            <input type="email" id="Email" required>
+                </br>
+            <input type="submit" value="Register">
+            </input>
+        </form>
+    </body>
